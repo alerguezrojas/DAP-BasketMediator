5820b9b42dd5882f71c7866076d88abe7ea944b8
Alejandro Rodríguez Rojas <alu0101317038@ull.edu.es>
2025-11-07 12:00:44 +0000

    CoachMediator

---
+++ src/basketball/CoachMediator.java
@@ -0,0 +1,22 @@
+package basketball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoachMediator implements Mediator {
+    private List<Player> players = new ArrayList<>();
+
+    public void registerPlayer(Player player) {
+        players.add(player);
+    }
+
+    @Override
+    public void sendMessage(String message, Player sender) {
+        for (Player player : players) {
+            // Notify all players except the sender
+            if (player != sender) {
+                player.receiveMessage(message);
+            }
+        }
+    }
+}
